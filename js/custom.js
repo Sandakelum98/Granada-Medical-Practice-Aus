df79094463dde793cb21c8e7c9cd46ee142f35f3
Sandakelum98 <sandakelumsanjaya@gmail.com>
2022-07-04 01:59:04 +0530

    home page

---
+++ js/custom.js
@@ -0,0 +1,43 @@
+(function ($) {
+    "use strict";
+    $(document).ready(function () {
+
+        $('.mobile-menu-toggle').on('click', function () {
+            $('.student-sidebar').addClass('show-sidebar');
+            $('body').toggleClass('menu-has-opened');
+        });
+
+        $('.schedule-tabs .schedule-tab').on('click', function () {
+            $('.time-table').hide();
+            var dataId = $(this).data('id');
+            $('#' + dataId).slideDown();
+            $('.schedule-tabs .schedule-tab').removeClass('active');
+            $(this).addClass('active');
+        });
+
+        $('.testimonial-slider').owlCarousel({
+            loop: true,
+            margin: 0,
+            nav: true,
+            autoplay: true,
+            autoplayHoverPause: true,
+            navText: '',
+            controls: true,
+            responsive: {
+                0: {
+                    items: 1
+                }
+            }
+        });
+
+        //testimonials slider
+        $('.testimonials-list').bxSlider({
+            auto: true,
+            autoControls: false,
+            stopAutoOnClick: true,
+            pager: false,
+            nextText: '<i class="fa fa-angle-right"></i>',
+            prevText: '<i class="fa fa-angle-left"></i>'
+        });
+    });
+})(jQuery);
